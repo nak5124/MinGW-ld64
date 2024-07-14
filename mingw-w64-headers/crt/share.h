bfb4a9259364e15422a92051ad0bfb4c7f2d2a8e
Yuta Nakai <nak5124@live.jp>
2024-07-14 16:01:34 +0900

    headers: corecrt_wio.h: Add new file and move some relevant declaration to it.

--- mingw-w64-headers/crt/share.h
+++ mingw-w64-headers/crt/share.h
@@ -7,8 +7,10 @@
 #define _INC_SHARE
 
 #ifndef _WIN32
-#error Only Win32 target is supported!
+# error Only Win32 target is supported!
 #endif
+
+#include <corecrt.h>
 
 #define _SH_COMPAT 0x00
 #define _SH_DENYRW 0x10
@@ -17,12 +19,10 @@
 #define _SH_DENYNO 0x40
 #define _SH_SECURE 0x80
 
-#ifndef	NO_OLDNAMES
 #define SH_COMPAT _SH_COMPAT
 #define SH_DENYRW _SH_DENYRW
 #define SH_DENYWR _SH_DENYWR
 #define SH_DENYRD _SH_DENYRD
 #define SH_DENYNO _SH_DENYNO
-#endif
 
-#endif
+#endif  /* _INC_SHARE */
