fec24dadb02f465d4275352c0487f8b74a33dadd
Yuta Nakai <nak5124@live.jp>
2024-08-06 21:25:49 +0900

    headers: sys/unistd.h: map to unistd.h instead of io.h.

--- mingw-w64-headers/crt/sys/unistd.h
+++ mingw-w64-headers/crt/sys/unistd.h
@@ -3,9 +3,4 @@
  * This file is part of the mingw-w64 runtime package.
  * No warranty is given; refer to the file DISCLAIMER.PD within this package.
  */
-/*
- * This file is part of the Mingw32 package.
- *
- * unistd.h maps (roughly) to io.h
- */
-#include <io.h>
+#include <unistd.h>
