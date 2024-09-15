mingw-w64/mingw-w64

headers: Avoid false-positive -Wconversion warnings in isnan() macro
Test case:

  #include <math.h>
  int isnan_wrapper(double value) { return isnan(value); }

Output:

  $ g++ -Wconversion -c snippet.cpp
  warning: conversion to 'float' from 'double' may alter its value
  warning: conversion to 'int' from 'double' may alter its value

First warning is caused by the fact in C++ mode __mingw_choose_expr is
using ternary operator and warning is triggered also by the case which is
not executed.

Second warning is caused by the fact that type of expression
(__builtin_trap(),x) for isnan(x) is double, but return value of
isnan_wrapper() is int. But this expression never returns.

Fix the first problem to use explicit cast for __isnan* calls which
correspondents to matched __mingw_types_compatible_p types.

And fix second problem by changing expression to something which has type
of int.

Bug: https://sourceforge.net/p/mingw-w64/bugs/481/
Signed-off-by: LIU Hao <lh_mouse@126.com>

mingw-w64-headers/crt/math.h
@@ -578,14 +578,14 @@ __mingw_choose_expr (                                         \
 #define isnan(x) \
 __mingw_choose_expr (                                         \
   __mingw_types_compatible_p (__typeof__ (x), double),            \
-    __isnan(x),                                                 \
+    __isnan((double)(x)),                                         \
     __mingw_choose_expr (                                     \
       __mingw_types_compatible_p (__typeof__ (x), float),         \
-        __isnanf(x),                                            \
+        __isnanf((float)(x)),                                     \
     __mingw_choose_expr (                                     \
       __mingw_types_compatible_p (__typeof__ (x), long double),   \
-        __isnanl(x),                                            \
-    __dfp_expansion(__isnan,(__builtin_trap(),x),x))))
+        __isnanl((long double)(x)),                               \
+    __dfp_expansion(__isnan,(__builtin_trap(),(int)0),x))))
 
 /* 7.12.3.5 */
 #define isnormal(x) (fpclassify(x) == FP_NORMAL)

nak5124/MinGW-ld64

mingw-w64-headers/crt/math.h
@@ -497,14 +497,14 @@ __MINGW_BEGIN_C_DECLS
 # define isnan(_X)                                                 \
     __mingw_choose_expr(                                           \
       __mingw_types_compatible_p(__typeof__(_X), double),          \
-      __isnan(_X),                                                 \
+      __isnan((double)(_X)),                                       \
       __mingw_choose_expr(                                         \
         __mingw_types_compatible_p(__typeof__(_X), float),         \
-        __isnanf(_X),                                              \
+        __isnanf((float)(_X)),                                     \
         __mingw_choose_expr(                                       \
           __mingw_types_compatible_p(__typeof__(_X), long double), \
-          __isnanl(_X),                                            \
-          (__builtin_trap(), _X))))
+          __isnanl((long double)(_X)),                             \
+          (__builtin_trap(), (int)0))))
 #endif
 
 /* 7.12.3.5 */