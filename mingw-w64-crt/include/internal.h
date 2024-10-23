mingw-w64/mingw-w64

crt: Split UCRT _amsg_exit() into separate file
Also add missing __MINGW_ATTRIB_NORETURN attribute into header file
internal.h where is function _amsg_exit() declared.

Signed-off-by: Martin Storsjö <martin@martin.st>

mingw-w64-crt/include/internal.h
@@ -102,7 +102,7 @@ extern "C" {
   _CRTIMP char *** __cdecl __p___initenv(void);
 #define __initenv (*__p___initenv())
 
-  _CRTIMP void __cdecl _amsg_exit(int);
+  _CRTIMP void __cdecl _amsg_exit(int) __MINGW_ATTRIB_NORETURN;
 
   int __CRTDECL _setargv(void);
   int __CRTDECL __setargv(void);

nak5124/MinGW-ld64

mingw-w64-crt/include/internal.h
@@ -92,7 +92,7 @@ extern "C" {
   _CRTIMP char *** __cdecl __p___initenv(void);
 #define __initenv (*__p___initenv())
 
-  _CRTIMP void __cdecl _amsg_exit(int);
+  _CRTIMP void __cdecl _amsg_exit(int) __MINGW_NORETURN;
 
   int __CRTDECL _setargv(void);
   int __CRTDECL __setargv(void);