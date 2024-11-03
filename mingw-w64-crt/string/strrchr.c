bd1b49bbc1ae8648cef6db0ad8678718fcdd1891
Yuta Nakai <nak5124@live.jp>
2024-11-03 18:07:49 +0900

    crt: Make __memrchr and __strchrnul static functions.

--- mingw-w64-crt/string/strrchr.c
+++ mingw-w64-crt/string/strrchr.c
@@ -23,7 +23,13 @@
 
 #include <string.h>
 
-void *__memrchr(const void *m, int c, size_t n);
+static void *__memrchr(const void *m, int c, size_t n)
+{
+	const unsigned char *s = m;
+	c = (unsigned char)c;
+	while (n--) if (s[n]==c) return (void *)(s+n);
+	return 0;
+}
 
 char *strrchr(const char *s, int c)
 {
