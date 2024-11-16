396e492349ce188145acd0cbb070c4f3da99d6fd
Yuta Nakai <nak5124@live.jp>
2024-11-16 21:17:24 +0900

    headers: Add (v)snwprintf_s.

--- mingw-w64-headers/crt/corecrt_wstdio.h
+++ mingw-w64-headers/crt/corecrt_wstdio.h
@@ -908,6 +908,31 @@ __MINGW_BEGIN_C_DECLS
     return _Ret;
   }
 
+#ifdef __MINGW_USE_SECAPI
+
+  __mingw_ovr
+  /* __attribute__((__format__(gnu_wprintf, 3, 0))) */ __MINGW_NONNULL((3))
+  int vsnwprintf_s(wchar_t *__restrict _Buffer, size_t _BufferCount, const wchar_t *__restrict _Format, va_list _ArgList)
+  {
+    int _Ret;
+    _Ret = __stdio_common_vswprintf(_CRT_INTERNAL_LOCAL_PRINTF_OPTIONS | _CRT_INTERNAL_PRINTF_STANDARD_SNPRINTF_BEHAVIOR, _Buffer, _BufferCount, _Format, NULL, _ArgList);
+    return _Ret < 0 ? -1 : _Ret;
+  }
+
+  __mingw_ovr
+  /* __attribute__((__format__(gnu_wprintf, 3, 4))) */ __MINGW_NONNULL((3))
+  int snwprintf_s(wchar_t *__restrict _Buffer, size_t _BufferCount, const wchar_t *__restrict _Format, ...)
+  {
+    __builtin_va_list _ArgList;
+    int _Ret;
+    __builtin_va_start(_ArgList, _Format);
+    _Ret = __stdio_common_vswprintf(_CRT_INTERNAL_LOCAL_PRINTF_OPTIONS | _CRT_INTERNAL_PRINTF_STANDARD_SNPRINTF_BEHAVIOR, _Buffer, _BufferCount, _Format, NULL, _ArgList);
+    __builtin_va_end(_ArgList);
+    return _Ret;
+  }
+
+#endif
+
 #if defined(__MINGW_USE_ISOC99) || defined(__MINGW_USE_C99FORGXX)
 
   __mingw_ovr
