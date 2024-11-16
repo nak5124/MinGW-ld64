17db20850f580d9ef99bc77374fd27ccb1bcfc19
Yuta Nakai <nak5124@live.jp>
2024-11-16 21:11:03 +0900

    headers: Fix vsnwprintf return value.

--- mingw-w64-headers/crt/corecrt_wstdio.h
+++ mingw-w64-headers/crt/corecrt_wstdio.h
@@ -892,7 +892,7 @@ __MINGW_BEGIN_C_DECLS
   int vsnwprintf(wchar_t *__restrict _Buffer, size_t _BufferCount, const wchar_t *__restrict _Format, va_list _ArgList)
   {
     int _Ret;
-    _Ret = __stdio_common_vswprintf(_CRT_INTERNAL_LOCAL_PRINTF_OPTIONS, _Buffer, _BufferCount, _Format, NULL, _ArgList);
+    _Ret = __stdio_common_vswprintf(_CRT_INTERNAL_LOCAL_PRINTF_OPTIONS | _CRT_INTERNAL_PRINTF_STANDARD_SNPRINTF_BEHAVIOR, _Buffer, _BufferCount, _Format, NULL, _ArgList);
     return _Ret < 0 ? -1 : _Ret;
   }
 
@@ -941,7 +941,9 @@ __MINGW_BEGIN_C_DECLS
   /* __attribute__((__format__(gnu_wprintf, 3, 0))) */ __MINGW_NONNULL((3))
   int vswprintf(wchar_t *__restrict _Buffer, size_t _BufferCount, const wchar_t *__restrict _Format, va_list _ArgList)
   {
-    return vsnwprintf(_Buffer, _BufferCount, _Format, _ArgList);
+    int _Ret;
+    _Ret = __stdio_common_vswprintf(_CRT_INTERNAL_LOCAL_PRINTF_OPTIONS, _Buffer, _BufferCount, _Format, NULL, _ArgList);
+    return _Ret < 0 ? -1 : _Ret;
   }
 
   __mingw_ovr
@@ -951,9 +953,9 @@ __MINGW_BEGIN_C_DECLS
     __builtin_va_list _ArgList;
     int _Ret;
     __builtin_va_start(_ArgList, _Format);
-    _Ret = vsnwprintf(_Buffer, _BufferCount, _Format, _ArgList);
+    _Ret = __stdio_common_vswprintf(_CRT_INTERNAL_LOCAL_PRINTF_OPTIONS, _Buffer, _BufferCount, _Format, NULL, _ArgList);
     __builtin_va_end(_ArgList);
-    return _Ret;
+    return _Ret < 0 ? -1 : _Ret;
   }
 
 #endif  /* defined(__MINGW_USE_ISOC95) || defined(__MINGW_USE_UNIX98) || defined(__MINGW_USE_C99FORGXX) */
@@ -966,7 +968,9 @@ extern "C++"
   /* __attribute__((__format__(gnu_wprintf, 2, 0))) */ __MINGW_NONNULL((2)) __MINGW_DEPRECATED_SEC_WARN
   int vswprintf(wchar_t *__restrict _Buffer, const wchar_t *__restrict _Format, va_list _ArgList)
   {
-    return _vswprintf(_Buffer, _Format, _ArgList);
+    int _Ret;
+    _Ret = __stdio_common_vswprintf(_CRT_INTERNAL_LOCAL_PRINTF_OPTIONS, _Buffer, (size_t)-1, _Format, NULL, _ArgList);
+    return _Ret < 0 ? -1 : _Ret;
   }
 
   __mingw_ovr
@@ -976,15 +980,17 @@ extern "C++"
     __builtin_va_list _ArgList;
     int _Ret;
     __builtin_va_start(_ArgList, _Format);
-    _Ret = vswprintf(_Buffer, _Format, _ArgList);
+    _Ret = __stdio_common_vswprintf(_CRT_INTERNAL_LOCAL_PRINTF_OPTIONS, _Buffer, (size_t)-1, _Format, NULL, _ArgList);
     __builtin_va_end(_ArgList);
-    return _Ret;
+    return _Ret < 0 ? -1 : _Ret;
   }
 
   __mingw_ovr __MINGW_DEPRECATED_SEC_WARN
   int _vswprintf_l(wchar_t *_Buffer, const wchar_t *_Format, _locale_t _Locale, va_list _ArgList)
   {
-    return _vswprintf_l(_Buffer, (size_t)-1, _Format, _Locale, _ArgList);
+    int _Ret;
+    _Ret = __stdio_common_vswprintf(_CRT_INTERNAL_LOCAL_PRINTF_OPTIONS, _Buffer, (size_t)-1, _Format, _Locale, _ArgList);
+    return _Ret < 0 ? -1 : _Ret;
   }
 
   __mingw_ovr __MINGW_DEPRECATED_SEC_WARN
