fea731325f0003452e6c72435f22711eb270a9d8
Yuta Nakai <nak5124@live.jp>
2024-11-22 23:15:38 +0900

    crt: Make realpath longpathaware.

--- mingw-w64-crt/misc/realpath.c
+++ mingw-w64-crt/misc/realpath.c
@@ -14,14 +14,8 @@
 #include <windows.h>
 #include <shlwapi.h>
 
-char *__cdecl realpath(const char *__restrict _Path, char *__restrict _Resolved_path)
+static char *__cdecl realpatha(const char *__restrict _Path, char *__restrict _Resolved_path)
 {
-    if(!_Path)
-    {
-        errno = EINVAL;
-        return NULL;
-    }
-
     if(!PathFileExistsA(_Path))
     {
         errno = ENOENT;
@@ -55,6 +49,71 @@ char *__cdecl realpath(const char *__restrict _Path, char *__restrict _Resolved_
         else
         {
             strcpy(_Resolved_path, buf);
+        }
+    }
+
+    return _Resolved_path;
+}
+
+char *__cdecl wrealpath(const char *__restrict _Path, char *__restrict _Resolved_path)
+{
+    if(!_Path)
+    {
+        errno = EINVAL;
+        return NULL;
+    }
+
+    int len0 = MultiByteToWideChar(CP_THREAD_ACP, 0, _Path, -1, NULL, 0);
+    wchar_t *wpath = (wchar_t *)malloc(len0 * sizeof(wchar_t));
+    int len1 = MultiByteToWideChar(CP_THREAD_ACP, 0, _Path, -1, wpath, len0);
+    if(len0 != len1)
+    {
+        free(wpath);
+        return realpatha(_Path, _Resolved_path);
+    }
+    else
+    {
+        if(!PathFileExistsW(wpath))
+        {
+            errno = ENOENT;
+            return NULL;
+        }
+        wchar_t wbuf[32767 * 2];
+        DWORD retval = GetFullPathNameW(wpath, 32767 * 2, wbuf, NULL);
+        free(wpath);
+
+        if(retval == 0)
+        {
+            if(GetLastError() == ERROR_FILENAME_EXCED_RANGE)
+            {
+                errno = ENAMETOOLONG;
+                return NULL;
+            }
+            else
+            {
+                errno = EIO;
+                return NULL;
+            }
+        }
+        else
+        {
+            char abuf[32767 * 4];
+            len0 = WideCharToMultiByte(CP_THREAD_ACP, 0, wbuf, -1, NULL, 0, NULL, NULL);
+            len1 = WideCharToMultiByte(CP_THREAD_ACP, 0, wbuf, -1, abuf, len0, NULL, NULL);
+            if(len0 != len1)
+            {
+                return realpatha(_Path, _Resolved_path);
+            }
+            else if(!_Resolved_path)
+            {
+                _Resolved_path = (char *)malloc(len0);
+                if(!_Resolved_path)
+                {
+                    errno = ENOMEM;
+                    return NULL;
+                }
+            }
+            strcpy(_Resolved_path, abuf);
         }
     }
 
