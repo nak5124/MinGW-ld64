mingw-w64/mingw-w64

headers: Use aarch64 intrinsic implementations for ARM64EC.
ARM64EC defines x86_64 identification macros, but its actual intrinsics are aarch64-based. Since this
differs from what x86_64 code expects, MSVC additionally provides softintrin.h (and softintrin.lib),
which act as wrappers to offer x86_64 variants of the intrinsics.

Signed-off-by: Jacek Caban <jacek@codeweavers.com>

mingw-w64-headers/include/winnt.h
@@ -1589,7 +1589,7 @@ __MINGW_CXX14_CONSTEXPR inline ENUMTYPE& operator ^= (ENUMTYPE& a, ENUMTYPE b) {
 
 #ifdef _AMD64_
 
-#if defined(__x86_64) && !defined(RC_INVOKED)
+#if defined(__x86_64) && !defined(__arm64ec__) && !defined(RC_INVOKED)
 
 #ifdef __cplusplus
   extern "C" {
@@ -1789,7 +1789,7 @@ extern "C" {
 #ifdef __cplusplus
   }
 #endif
-#endif /* defined(__x86_64) && !defined(RC_INVOKED) */
+#endif /* defined(__x86_64) && !defined(__arm64ec__) && !defined(RC_INVOKED) */
 
 #define EXCEPTION_READ_FAULT 0
 #define EXCEPTION_WRITE_FAULT 1
@@ -2154,9 +2154,9 @@ extern "C" {
 #endif /* _ARM_ */
 
 
-#ifdef _ARM64_
+#if defined(_ARM64_) || defined(_ARM64EC_)
 
-#if defined(__aarch64__) && !defined(RC_INVOKED)
+#if (defined(__aarch64__) || defined(__arm64ec__)) && !defined(RC_INVOKED)
 
 #ifdef __cplusplus
   extern "C" {
@@ -2226,7 +2226,7 @@ extern "C" {
 #ifdef __cplusplus
   }
 #endif
-#endif /* defined(__aarch64__) && !defined(RC_INVOKED) */
+#endif /* (defined(__aarch64__) || defined(__arm64ec__)) && !defined(RC_INVOKED) */
 
 #define EXCEPTION_READ_FAULT    0
 #define EXCEPTION_WRITE_FAULT   1
@@ -9120,7 +9120,7 @@ typedef DWORD (WINAPI *PRTL_RUN_ONCE_INIT_FN)(PRTL_RUN_ONCE, PVOID, PVOID *);
 #if !defined (__CRT__NO_INLINE) && !defined (__WIDL__)
     __CRT_INLINE PVOID WINAPI RtlSecureZeroMemory(PVOID ptr,SIZE_T cnt) {
       volatile char *vptr =(volatile char *)ptr;
-#ifdef __x86_64
+#if defined(__x86_64__) && !defined(__arm64ec__)
       __stosb((PBYTE)((DWORD64)vptr),0,cnt);
 #else
       while(cnt) {

nak5124/MinGW-ld64

mingw-w64-headers/include/winnt.h
@@ -1607,7 +1607,7 @@ extern "C" {
 
 #ifdef _AMD64_
 
-#if defined(__x86_64__) && !defined(RC_INVOKED)
+#if defined(__x86_64__) && !defined(__arm64ec__) && !defined(RC_INVOKED)
 
 #ifdef __cplusplus
 extern "C" {
@@ -2030,7 +2030,7 @@ extern "C" {
 }
 #endif
 
-#endif  /* defined(__x86_64__) && !defined(RC_INVOKED) */
+#endif  /* defined(__x86_64__) && !defined(__arm64ec__) && !defined(RC_INVOKED) */
 
 #define EXCEPTION_READ_FAULT    0
 #define EXCEPTION_WRITE_FAULT   1
@@ -2242,9 +2242,9 @@ extern "C" {
     } ScopeRecord[1];
   } SCOPE_TABLE_ARM64, *PSCOPE_TABLE_ARM64;
 
-#ifdef _ARM64_
+#if defined(_ARM64_) || defined(_ARM64EC_)
 
-#if defined(__aarch64__) && !defined(RC_INVOKED)
+#if (defined(__aarch64__) || defined(__arm64ec__)) && !defined(RC_INVOKED)
 
 #ifdef __cplusplus
 extern "C" {
@@ -2505,7 +2505,7 @@ extern "C" {
 }
 #endif
 
-#endif  /* defined(__aarch64__) && !defined(RC_INVOKED) */
+#endif  /* (defined(__aarch64__) || defined(__arm64ec__)) && !defined(RC_INVOKED) */
 
 #define EXCEPTION_READ_FAULT    0
 #define EXCEPTION_WRITE_FAULT   1
@@ -10942,7 +10942,7 @@ extern "C" {
   PVOID RtlSecureZeroMemory(PVOID ptr, SIZE_T cnt)
   {
     volatile char *vptr =(volatile char *)ptr;
-#ifdef __x86_64__
+#if defined(__x86_64__) && !defined(__arm64ec__)
     __stosb((PBYTE)((DWORD64)vptr), 0, cnt);
 #else  /* __aarch64__ */
     while(cnt)