39d009ef4ab8d4cf45cf9ffa3766f5358a2040a3
Yuta Nakai <nak5124@live.jp>
2024-12-28 23:34:01 +0900

    crt: Enable realpath to resolve symbolic links.

--- mingw-w64-crt/misc/realpath.c
+++ mingw-w64-crt/misc/realpath.c
@@ -12,22 +12,19 @@
 #define NOMCX
 #define NOIME
 #include <windows.h>
-#include <shlwapi.h>
 
 static char *__cdecl realpatha(const char *__restrict _Path, char *__restrict _Resolved_path)
 {
-    if(!PathFileExistsA(_Path))
+    HANDLE hFile = CreateFileA(_Path, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+    if(hFile == INVALID_HANDLE_VALUE)
     {
-        errno = ENOENT;
-        return NULL;
-    }
-
-    char buf[MAX_PATH * 4];
-    DWORD retval = GetFullPathNameA(_Path, MAX_PATH * 4, !_Resolved_path ? buf : _Resolved_path, NULL);
-
-    if(retval == 0)
-    {
-        if(GetLastError() == ERROR_FILENAME_EXCED_RANGE)
+        DWORD err = GetLastError();
+        if(err == ERROR_FILE_NOT_FOUND)
+        {
+            errno = ENOENT;
+            return NULL;
+        }
+        else if(err == ERROR_FILENAME_EXCED_RANGE)
         {
             errno = ENAMETOOLONG;
             return NULL;
@@ -38,24 +35,45 @@ static char *__cdecl realpatha(const char *__restrict _Path, char *__restrict _R
             return NULL;
         }
     }
-    else if(!_Resolved_path)
+
+    char buf[MAX_PATH * 4];
+    DWORD retval = GetFinalPathNameByHandleA(hFile, buf, MAX_PATH * 4, FILE_NAME_NORMALIZED | VOLUME_NAME_DOS);
+    CloseHandle(hFile);
+    if(retval == 0)
     {
-        _Resolved_path = (char *)malloc(retval + 1);
-        if(!_Resolved_path)
+        DWORD err = GetLastError();
+        if(err == ERROR_PATH_NOT_FOUND)
+        {
+            errno = ENOENT;
+            return NULL;
+        }
+        else if(err == ERROR_NOT_ENOUGH_MEMORY)
         {
             errno = ENOMEM;
             return NULL;
         }
         else
         {
-            strcpy(_Resolved_path, buf);
+            errno = EIO;
+            return NULL;
         }
     }
+
+    if(!_Resolved_path)
+    {
+        _Resolved_path = (char *)malloc(retval - 4);
+        if(!_Resolved_path)
+        {
+            errno = ENOMEM;
+            return NULL;
+        }
+    }
+    strcpy(_Resolved_path, buf + 4);
 
     return _Resolved_path;
 }
 
-char *__cdecl wrealpath(const char *__restrict _Path, char *__restrict _Resolved_path)
+char *__cdecl realpath(const char *__restrict _Path, char *__restrict _Resolved_path)
 {
     if(!_Path)
     {
@@ -63,59 +81,81 @@ char *__cdecl wrealpath(const char *__restrict _Path, char *__restrict _Resolved
         return NULL;
     }
 
-    int len0 = MultiByteToWideChar(CP_THREAD_ACP, 0, _Path, -1, NULL, 0);
+    int      len0  = MultiByteToWideChar(CP_THREAD_ACP, 0, _Path, -1, NULL, 0);
     wchar_t *wpath = (wchar_t *)malloc(len0 * sizeof(wchar_t));
-    int len1 = MultiByteToWideChar(CP_THREAD_ACP, 0, _Path, -1, wpath, len0);
+    int      len1  = MultiByteToWideChar(CP_THREAD_ACP, 0, _Path, -1, wpath, len0);
     if(len0 != len1)
     {
         free(wpath);
         return realpatha(_Path, _Resolved_path);
     }
-    else
+
+    CREATEFILE2_EXTENDED_PARAMETERS extendedParams = { 0 };
+    extendedParams.dwSize           = sizeof(CREATEFILE2_EXTENDED_PARAMETERS);
+    extendedParams.dwFileAttributes = FILE_ATTRIBUTE_NORMAL;
+    HANDLE hFile = CreateFile2(wpath, GENERIC_READ, FILE_SHARE_READ, OPEN_EXISTING, &extendedParams);
+    free(wpath);
+    if(hFile == INVALID_HANDLE_VALUE)
     {
-        if(!PathFileExistsW(wpath))
+        DWORD err = GetLastError();
+        if(err == ERROR_FILE_NOT_FOUND)
         {
             errno = ENOENT;
             return NULL;
         }
-        wchar_t wbuf[32767 * 2];
-        DWORD retval = GetFullPathNameW(wpath, 32767 * 2, wbuf, NULL);
-        free(wpath);
-
-        if(retval == 0)
+        else if(err == ERROR_FILENAME_EXCED_RANGE)
         {
-            if(GetLastError() == ERROR_FILENAME_EXCED_RANGE)
-            {
-                errno = ENAMETOOLONG;
-                return NULL;
-            }
-            else
-            {
-                errno = EIO;
-                return NULL;
-            }
+            errno = ENAMETOOLONG;
+            return NULL;
         }
         else
         {
-            char abuf[32767 * 4];
-            len0 = WideCharToMultiByte(CP_THREAD_ACP, 0, wbuf, -1, NULL, 0, NULL, NULL);
-            len1 = WideCharToMultiByte(CP_THREAD_ACP, 0, wbuf, -1, abuf, len0, NULL, NULL);
-            if(len0 != len1)
-            {
-                return realpatha(_Path, _Resolved_path);
-            }
-            else if(!_Resolved_path)
-            {
-                _Resolved_path = (char *)malloc(len0);
-                if(!_Resolved_path)
-                {
-                    errno = ENOMEM;
-                    return NULL;
-                }
-            }
-            strcpy(_Resolved_path, abuf);
+            errno = EIO;
+            return NULL;
         }
     }
+
+    wchar_t wbuf[32767 * 2];
+    DWORD retval = GetFinalPathNameByHandleW(hFile, wbuf, 32767 * 2, FILE_NAME_NORMALIZED | VOLUME_NAME_DOS);
+    CloseHandle(hFile);
+    if(retval == 0)
+    {
+        DWORD err = GetLastError();
+        if(err == ERROR_PATH_NOT_FOUND)
+        {
+            errno = ENOENT;
+            return NULL;
+        }
+        if(err == ERROR_NOT_ENOUGH_MEMORY)
+        {
+            errno = ENOMEM;
+            return NULL;
+        }
+        else
+        {
+            errno = EIO;
+            return NULL;
+        }
+    }
+
+    char abuf[32767 * 4];
+    len0 = WideCharToMultiByte(CP_THREAD_ACP, 0, wbuf, -1, NULL, 0, NULL, NULL);
+    len1 = WideCharToMultiByte(CP_THREAD_ACP, 0, wbuf, -1, abuf, len0, NULL, NULL);
+    if(len0 != len1)
+    {
+        return realpatha(_Path, _Resolved_path);
+    }
+
+    if(!_Resolved_path)
+    {
+        _Resolved_path = (char *)malloc(len0 - 4);
+        if(!_Resolved_path)
+        {
+            errno = ENOMEM;
+            return NULL;
+        }
+    }
+    strcpy(_Resolved_path, abuf + 4);
 
     return _Resolved_path;
 }
