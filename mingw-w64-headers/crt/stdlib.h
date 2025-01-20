b6fa229530169c03fd73b9283a432b6917acf14e
Yuta Nakai <nak5124@live.jp>
2025-01-20 21:45:11 +0900

    headers: Enable qsort_r when _POSIX_C_SOURCE>=202405L or _XOPEN_SOURCE>=800.
    Signed-off-by: Yuta Nakai <nak5124@live.jp>

--- mingw-w64-headers/crt/stdlib.h
+++ mingw-w64-headers/crt/stdlib.h
@@ -560,7 +560,7 @@ __MINGW_BEGIN_C_DECLS
   extern int __cdecl clearenv(void) __NOTHROW;
 #endif
 
-#ifdef __MINGW_USE_GNU
+#ifdef __MINGW_USE_XOPEN2K24
   typedef int (__cdecl *_Compare_fn_t)(const void *, const void *, void *);
   extern void __cdecl qsort_r(void *_Base, size_t _Nmemb, size_t _Size, _Compare_fn_t _Compar, void *_Arg) __NONNULL((1, 4));
 #endif
