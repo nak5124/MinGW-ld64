319024e42a48c309a30acbb25247dd10d57732e4
Yuta Nakai <nak5124@live.jp>
2025-02-02 01:00:42 +0900

    crt: Move stdio/truncate.c to misc/ and split into truncate.c and truncate64.c.
    Signed-off-by: Yuta Nakai <nak5124@live.jp>

---
+++ mingw-w64-crt/misc/truncate64.c
@@ -0,0 +1,25 @@
+#include <fcntl.h>
+#include <errno.h>
+
+int __cdecl truncate64(const char *__file, _off64_t __length);
+int __cdecl ftruncate64(int __fd, _off64_t __length);
+
+int __cdecl truncate64(const char *__file, _off64_t __length)
+{
+  int ret, fd;
+  errno_t err = _sopen_s(&fd, __file, _O_BINARY | _O_RDWR, _SH_DENYNO, 0);
+  if(fd == -1)
+  {
+    /* errno is set by _sopen_s */
+    return -1;
+  }
+  ret = ftruncate64(fd, __length);
+  err = errno;
+  if(_close(fd) == -1)
+  {
+    /* errno is set by _close: EBADF */
+    return -1;
+  }
+  errno = err;
+  return ret;
+}
