283f4dadbc67a18facc26dc02a6fe69ac4d30d99
Pali Rohár <pali.rohar@gmail.com>
2025-02-07 13:01:08 +0100

    crt: Remove DllEntryPoint from DLL builds
    Function with name DllEntryPoint is entry point of the DLL library,
    an alternative or aliased name to DllMainCRTStartup. So calling entry point
    (DllEntryPoint) from entry point (DllMainCRTStartup) is just wrong.
    
    Default DLL entry point function varies between compilers/linkers.
    Borland C uses DllEntryPoint, GNU LD uses DllMainCRTStartup, MSVC link.exe
    uses _DllMainCRTStartup.
    
    The DLL application start function has same name DllMain.
    
    Anyway, if the application is using own custom DLL entry point then it has
    to pass appropriate compiler / linker parameters to omit inclusion of
    standard library and/or startup files. Therefore removal of DllEntryPoint
    should not cause big issues.
    
    Signed-off-by: Martin Storsjö <martin@martin.st>

--- mingw-w64-crt/crt/crtdll.c
+++ mingw-w64-crt/crt/crtdll.c
@@ -34,8 +34,6 @@ static _onexit_table_t atexit_table;
 
 extern int __mingw_app_type;
 
-extern WINBOOL WINAPI DllEntryPoint (HANDLE, DWORD, LPVOID);
-
 WINBOOL WINAPI _CRT_INIT (HANDLE hDllHandle, DWORD dwReason, LPVOID lpreserved)
 {
   if (dwReason == DLL_PROCESS_DETACH)
@@ -150,13 +148,6 @@ DllMainCRTStartup (HANDLE hDllHandle, DWORD dwReason, LPVOID lpreserved)
         retcode = _CRT_INIT (hDllHandle, dwReason, lpreserved);
         if (!retcode)
           goto i__leave;
-        retcode = DllEntryPoint (hDllHandle, dwReason, lpreserved);
-	if (! retcode)
-	  {
-	    if (dwReason == DLL_PROCESS_ATTACH)
-	      _CRT_INIT (hDllHandle, DLL_PROCESS_DETACH, lpreserved);
-	    goto i__leave;
-	  }
     }
 #ifdef __USING_MCFGTHREAD__
   if (dwReason == DLL_PROCESS_ATTACH)
@@ -170,14 +161,11 @@ DllMainCRTStartup (HANDLE hDllHandle, DWORD dwReason, LPVOID lpreserved)
   if (dwReason == DLL_PROCESS_ATTACH && ! retcode)
     {
 	DllMain (hDllHandle, DLL_PROCESS_DETACH, lpreserved);
-	DllEntryPoint (hDllHandle, DLL_PROCESS_DETACH, lpreserved);
 	_CRT_INIT (hDllHandle, DLL_PROCESS_DETACH, lpreserved);
     }
   if (dwReason == DLL_PROCESS_DETACH || dwReason == DLL_THREAD_DETACH)
     {
-        retcode = DllEntryPoint (hDllHandle, dwReason, lpreserved);
-	if (_CRT_INIT (hDllHandle, dwReason, lpreserved) == FALSE)
-	  retcode = FALSE;
+	retcode = _CRT_INIT (hDllHandle, dwReason, lpreserved);
     }
 #ifdef __USING_MCFGTHREAD__
   if (dwReason == DLL_PROCESS_DETACH && lpreserved == NULL)
