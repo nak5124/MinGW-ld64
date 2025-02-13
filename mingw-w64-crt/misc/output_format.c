26482d71639135b1b902797ee21dfdc1fd101d4d
Yuta Nakai <nak5124@live.jp>
2025-02-14 00:00:04 +0900

    crt: Remove unnecessary #define/#undef.
    Signed-off-by: Yuta Nakai <nak5124@live.jp>

--- mingw-w64-crt/misc/output_format.c
+++ mingw-w64-crt/misc/output_format.c
@@ -1,9 +1,9 @@
-#define _get_output_format __dummy__get_output_format
-#define _set_output_format __dummy__set_output_format
+/**
+ * This file has no copyright assigned and is placed in the Public Domain.
+ * This file is part of the mingw-w64 runtime package.
+ * No warranty is given; refer to the file DISCLAIMER.PD within this package.
+ */
 #include <windows.h>
-
-#undef _get_output_format
-#undef _set_output_format
 
 static unsigned int last_value = 0;
 typedef unsigned int (*f_get_output_format)(void);
